d784c696151b0bef4a9d49ec3b6c53ce1c87d4d3
bruno-sm <bru.1bruno@gmail.com>
2018-12-23 17:40:18 +0100

    Añade el tipo expresión

--- src/ast.ml
+++ src/ast.ml
@@ -6,8 +6,10 @@ type expression = Sequence of expression list
                 | Declaration of string * type_expression
                 | Assignment of string * expression
                 | If of expression * expression *
-                        (expression * expression) list *
+                        expression list *
                         optional expression
+                | Elif of expression * expression
+                | Else of expression
                 | Type of type_expression
                 | IntLiteral of int
                 | RealLiteral of float
@@ -18,49 +20,88 @@ type expression = Sequence of expression list
                 | ListLiteral of expression list
                 | MatrixLiteral of expression list list
 
-(*
-let string_of_expression = function
-  | Sequence exp_list -> Printf.sprintf "Sequence\n\t| %s" @@
+
+let rec string_of_expression = function
+  | Sequence exp_list -> Printf.sprintf "Sequence\n\t| %s\n" @@
                          List.map string_of_expression exp_list |>
                          List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) ""
 
-  | Parenthesis e -> Printf.sprintf "Parenthesis\n\t| %s" @@ string_of_expression e
+  | Parenthesis e -> Printf.sprintf "Parenthesis\n\t| %s\n" @@ string_of_expression e
 
-  | Block e -> Printf.sprintf "Block\n\t| %s" @@ string_of_expression e
+  | Block e -> Printf.sprintf "Block\n\t| %s\n" @@ string_of_expression e
 
-  | Lambda v t e -> Printf.sprintf "Lambda(%s)\n\t| %s\n\t| %s"
+  | Lambda v t e -> Printf.sprintf "Lambda(%s)\n\t| %s\n\t| %s\n"
                     (string_of_expression v)
                     (string_of_expression t)
                     (string_of_expression e)
 
-  | FunctionCall name arg -> Printf.sprintf "FunctionCall(%s)\n\t| %s"
+  | FunctionCall name arg -> Printf.sprintf "FunctionCall(%s)\n\t| %s\n"
                              (string_of_expression name)
                              (string_of_expression arg)
 
-  | Declaration name t -> Printf.sprintf "Declaration(%s)\n\t| %s"
+  | Declaration name t -> Printf.sprintf "Declaration(%s)\n\t| %s\n"
                           (string_of_expression name)
                           (string_of_expression t)
 
-  | Assignment name e -> Printf.sprintf "Assignment(%s)\n\t| %s"
+  | Assignment name e -> Printf.sprintf "Assignment(%s)\n\t| %s\n"
                          (string_of_expression name)
                          (string_of_expression e)
 
-  | If cond do_if [] None -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t)"
+  | If cond do_if [] None -> Printf.sprintf "If\n\t| %s\n\t| %s\n"
                              (string_of_expression cond)
                              (string_of_expression do_if)
 
-  | If cond do_if elif_list None -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| %s)"
+  | If cond do_if elif_list None -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| %s\n)"
                                     (string_of_expression cond)
                                     (string_of_expression do_if)
-                                    (List.map (fun cond e -> Printf.sprintf "Elif\n\t| %s\n\t| %s"
-                                                             (strig_of_expression cond)
-                                                             (string_of_expression e))
-                                              elif_list |>
+                                    (List.map string_of_expression elif_list |>
                                      List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) "")
 
-  | If cond do_if [] Some(do_else) -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| Else\n\t| %s)"
-  | If cond do_if elif_list Some(do_else) -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| Elif\n\t| %s \n\t| Else\n\t| %s)"
-*)
+  | If cond do_if [] Some(do_else) -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| %s\n"
+                                      (string_of_expression cond)
+                                      (string_of_expression do_if)
+                                      (string_of_expression do_else)
+
+  | If cond do_if elif_list Some(do_else) -> Printf.sprintf "If\n\t| %s\n\t| %s\n\t| %s \n\t| %s\n"
+                                             (string_of_expression cond)
+                                             (string_of_expression do_if)
+                                             (List.map string_of_expression elif_list |>
+                                              List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) "")
+                                             (string_of_expression do_else)
+
+  | Elif cond do_if -> Printf.sprintf "Elif\n\t| %s\n\t| %s\n"
+                                      (string_of_expression cond)
+                                      (string_of_expression do_if)
+
+  | Else do_else -> Printf.sprintf "Else\n\t| %s\n" @@ string_of_expression do_else
+
+  | Type t -> Printf.sprintf "Type\n\t| %s\n" @@ string_of_type_expression t
+
+  | IntLiteral n -> Printf.sprintf "IntLiteral\n\t| %d\n" n
+
+  | RealLiteral x -> Printf.sprintf "RealLiteral\n\t| %f\n" x
+
+  | StringLiteral s -> Printf.sprintf "StringLiteral\n\t| %s\n" s
+
+  | AtomLiteral a -> Printf.sprintf "AtomLiteral\n\t| %s\n" a
+
+  | BoolLiteral true -> "BoolLiteral\n\t| true\n"
+
+  | BoolLiteral false -> "BoolLiteral\n\t| false\n"
+
+  | TupleLiteral exp_list -> Printf.sprintf "TupleLiteral\n\t| %s\n" @@
+                             List.map string_of_expression exp_list |>
+                             List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) ""
+
+  | ListLiteral exp_list -> Printf.sprintf "ListLiteral\n\t| %s\n" @@
+                            List.map string_of_expression exp_list |>
+                            List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) ""
+
+  | MatrixLiteral exp_matrix -> Printf.sprintf "MatrixLiteral\n\t| %s\n" @@
+                              List.map (List.map string_of_expression) exp_matrix |>
+                              List.map (List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) "") |>
+                              List.fold_left (fun s1 s2 -> s1 ^ "\n\t| " ^ s2) ""
+
 
 type type_expression = Int
                      | Real
@@ -77,3 +118,43 @@ type type_expression = Int
                      | Union of type_expression * type_expression
                      | Intersection of type_expression * type_expression
                      | Complement of type_expression
+
+
+let rec string_of_type_expression = function
+  | Int -> "Int"
+
+  | Real -> "Real"
+
+  | String -> "String"
+
+  | Atom -> "Atom"
+
+  | SpecificAtom s -> Printf.sprintf "SpecificAtom(%s)" s
+
+  | Bool -> "Bool"
+
+  | Unknown -> "Unknown"
+
+  | Universe i -> Printf.sprintf "Universe(%d)" i
+
+  | Arrow source target -> Printf.sprintf "Arrow(%s, %s)"
+                           (string_of_type_expression source)
+                           (string_of_type_expression target)
+
+  | Tuple types -> Printf.sprintf("Tuple(%s)") @@
+                   List.map string_of_type_expression types |>
+                   List.fold_left (fun s1 s2 -> s1 ^ ", " ^ s2) ""
+
+  | List t -> Printf.sprintf("List(%s)") @@ string_of_type_expression t
+
+  | Matrix t -> Printf.sprintf("Matrix(%s)") @@ string_of_type_expression t
+
+  | Union t1 t2 -> Printf.sprintf("Union(%s, %s)")
+                   (string_of_type_expression t1)
+                   (string_of_type_expression t2)
+
+  | Intersection t1 t2 -> Printf.sprintf("Intersection(%s, %s)")
+                          (string_of_type_expression t1)
+                          (string_of_type_expression t2)
+
+  | Complement t -> Printf.sprintf("Complement(%s)") @@ string_of_type_expression t
